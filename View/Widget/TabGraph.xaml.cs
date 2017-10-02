78df3538396043025513eda1685e66bfd1af30f9
cvillalobos <villalobos.cristian@hotmail.es>
2017-10-02 16:26:37 -0300

    charts

--- View/Widget/TabGraph.xaml.cs
+++ View/Widget/TabGraph.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,64 +12,101 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using LiveCharts;
-using LiveCharts.Wpf;
 
 namespace cl.uv.leikelen.View.Widget
 {
     /// <summary>
     /// Lógica de interacción para TabGraph.xaml
     /// </summary>
-    public partial class TabGraph : TabItem
+    public partial class TabGraph : TabItem, INotifyPropertyChanged
     {
+        private bool _SeatedSeriesVisibility;
+        private bool _HandOpenSeriesVisibility;
+        private bool _HandDownSeriesVisibility;
+        private bool _PointSeriesVisibility;
+        private bool _StandedSeriesVisibility;
+        private bool _ArmCrossSeriesVisibility;
+
         public TabGraph()
         {
             InitializeComponent();
-            SeriesCollection = new SeriesCollection
-            {
-                new StackedRowSeries
-                {
-                    Title = "Seated",
-                    Values = new ChartValues<int> {25, 20, 30, 15},
-                    StackMode = StackMode.Values, // this is not necessary, values is the default stack mode
-                    DataLabels = true
-                },
-                new StackedRowSeries
-                {
-                    Title = "Hand Open",
-                    Values = new ChartValues<int> {25,7 , 40, 15},
-                    StackMode = StackMode.Values,
-                    DataLabels = true
-                },
-                new StackedRowSeries
-                {
-                    Title = "Standed",
-                    Values = new ChartValues<int> {25, 10, 15, 10},
-                    StackMode = StackMode.Values,
-                    DataLabels = true,                      
-                },
-            };
 
+            SeatedSeriesVisibility = true;
+            HandOpenSeriesVisibility = true;
+            HandDownSeriesVisibility = false;
+            PointSeriesVisibility = false;
+            StandedSeriesVisibility = false;
+            ArmCrossSeriesVisibility = false;
 
-            /*
-            //adding series updates and animates the chart
-            SeriesCollection.Add(new StackedRowSeries
-            {
-                Values = new ChartValues<double> { 6, 2, 7 },
-                StackMode = StackMode.Values
-            });
-            
-            //adding values also updates and animates
-            SeriesCollection[2].Values.Add(4d);            
-            */
-            Labels = new[] { "Person 1", "Person 2", "Person 3", "Person 4" };
-            YFormatter = value => value + " Horas";
             DataContext = this;
-
+        }
+        public bool SeatedSeriesVisibility
+        {
+            get { return _SeatedSeriesVisibility; }
+            set
+            {
+                _SeatedSeriesVisibility = value;
+                OnPropertyChanged("SeatedSeriesVisibility");
+            }
         }
 
-        public SeriesCollection SeriesCollection { get; set; }
-        public string[] Labels { get; set; }
-        public Func<double, string> YFormatter { get; set; }
+        public bool HandOpenSeriesVisibility
+        {
+            get { return _HandOpenSeriesVisibility; }
+            set
+            {
+                _HandOpenSeriesVisibility = value;
+                OnPropertyChanged("HandOpenSeriesVisibility");
+            }
+        }
+
+        public bool HandDownSeriesVisibility
+        {
+            get { return _HandDownSeriesVisibility; }
+            set
+            {
+                _HandDownSeriesVisibility = value;
+                OnPropertyChanged("HandDownSeriesVisibility");
+            }
+        }
+
+        public bool PointSeriesVisibility
+        {
+            get { return _PointSeriesVisibility; }
+            set
+            {
+                _PointSeriesVisibility = value;
+                OnPropertyChanged("PointSeriesVisibility");
+            }
+        }
+
+        public bool StandedSeriesVisibility
+        {
+            get { return _StandedSeriesVisibility; }
+            set
+            {
+                _StandedSeriesVisibility = value;
+                OnPropertyChanged("StandedSeriesVisibility");
+            }
+        }
+
+        public bool ArmCrossSeriesVisibility
+        {
+            get { return _ArmCrossSeriesVisibility; }
+            set
+            {
+                _ArmCrossSeriesVisibility = value;
+                OnPropertyChanged("ArmCrossSeriesVisibility");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
